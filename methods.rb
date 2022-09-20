d7a2c94d8c344ed5d7fa9d0abb25dacf525bfe82
Teejay Dixon <tahjgdixon@gmail.com>
2022-09-20 07:13:40 -0700

    commit tree a701c9512f1a1abf15f4e9970996d4dde64dd3e4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet "Jimmy"
+
+
+
+
+def greet_with_default name = "programmer"
+  puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+
+
+def add(num1, num2) 
+  return num1 + num2 
+end
+
+add(2,2)
+
+def halve(num1)
+  if num1 != "num1" 
+    puts nil
+  else
+    return num1/2
+  end
+end
+
+  halve(4)
